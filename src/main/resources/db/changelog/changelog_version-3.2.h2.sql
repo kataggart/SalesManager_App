4489742f96a1442187add88a6c11a4e193efc00e
kataggart <karen.a.taggart@gmail.com>
2022-06-21 17:17:27 -0400

    add vegan count item

--- src/main/resources/db/changelog/changelog_version-3.2.h2.sql
+++ src/main/resources/db/changelog/changelog_version-3.2.h2.sql
@@ -11,3 +11,7 @@ INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Leather sofa By Michellotti'
 --changeset JohnD:1582471835439-6
 INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Round coffee table By Michellotti', 1, 800.23);
 --rollback DELETE FROM SALES WHERE ITEM='Round coffee table By Michellotti';
+
+--changeset KT:1
+INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('vegan couch', 5, 9000.28);
+--rollback DELETE FROM SALES WHERE ITEM='vegan couch';
